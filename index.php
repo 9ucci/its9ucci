19db23b9c437773c62cf2a1f15ac796a8adf72f1
nmonty9 <namontalbano@email.neit.edu>
2017-12-22 15:45:40 -0500

    added files

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php
-$myFile = "testFile.txt";
-$fh = fopen($myFile, 'a') or die("can't open file");
-$stringData = $_POST['content']."\n";
-fwrite($fh, $stringData);
-fclose($fh);
+echo "hello";
 ?>
